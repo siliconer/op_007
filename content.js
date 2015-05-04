7a5c583c6b261ccc30fe63bd3c3c364ab6f442a8
xgg <siliconexplorer1@gmail.com>
2015-05-04 22:39:43 +0800

    j leauge

--- content.js
+++ content.js
@@ -19,6 +19,8 @@ function adjust() {
 	low_init_tie = low_init_data.querySelectorAll("td")[5].childNodes[0].nodeValue
 	low_init_lose = low_init_data.querySelectorAll("td")[6].childNodes[0].nodeValue
 
+	league_name =  document.querySelector('.o_l_name2').childNodes[1].nodeValue 
+
 	for (index = 0;index < company_length;index ++ ){
 		each_data = data.querySelectorAll("tr")[index]
 		// init win/tie/lose 2:3:4
@@ -35,22 +37,26 @@ function adjust() {
 		odds_data[index][9] = each_data.querySelectorAll("td")[10].childNodes[0].nodeValue;
 		odds_data[index][10] = each_data.querySelectorAll("td")[11].childNodes[0].nodeValue;
 		odds_data[index][11] = each_data.querySelectorAll("td")[12].childNodes[0].nodeValue;
+
 		console.log(odds_data[index][0]);
 		// each_data.querySelectorAll("td")[8].style.backgroundColor ='Yellow';
 		if (odds_data[index][0] == '立博(英国)') {
 			console.log('gotcha libo');
 			libo_index = index;
-			libo_init_win = odds_data[index][1]
-			libo_init_tie = odds_data[index][2]
-			libo_init_lose = odds_data[index][3]
-			libo_win_per = odds_data[index][4]
-			libo_tie_per  = odds_data[index][5]
-			libo_lose_per = odds_data[index][6]
-			libo_return_rate = odds_data[index][7]
-			libo_win_kelly = odds_data[index][8]
-			libo_tie_kelly= odds_data[index][9]
-			libo_lose_kelly = odds_data[index][10]	
+			libo_init_win = odds_data[index][1] ;
+			libo_init_tie = odds_data[index][2] ;
+			libo_init_lose = odds_data[index][3];
+			libo_win_per = odds_data[index][4];
+			libo_tie_per  = odds_data[index][5];
+			libo_lose_per = odds_data[index][6];
+			libo_return_rate = odds_data[index][7];
+			libo_win_kelly = odds_data[index][8];
+			libo_tie_kelly= odds_data[index][9];
+			libo_lose_kelly = odds_data[index][10];
 		}
+		company_name = odds_data[index][0] ;
+		league_adjust_company(company_name,league_name,each_data);
+
 	}
 	libo_data(libo_win_per,high_init_win,low_init_win,libo_index,5)
 	libo_data(libo_tie_per,high_init_tie,low_init_tie,libo_index,6)
@@ -68,8 +74,74 @@ function adjust() {
 	//footer part
 
 
+	//the tile part :
+	//j 联赛 Idaaa Sazaka 澳门 GLB victorchandler egobet
+
+
+
+
 }
 
+
+function league_adjust_company(company_name,league_name) {
+
+	if (company_name == 'iddaa(土耳其)') {
+		if ((league_name == '日職聯' )){ 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+	if (company_name == '澳门') {
+		if (league_name == '日職聯' ) { 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+
+	if (company_name == 'Sazaka') {
+		if ((league_name == '日職聯' )){ 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+
+	if (company_name == 'GLB') {
+		if ((league_name == '日職聯' )){ 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+
+	if (company_name == 'Victor Chandler') {
+		if ((league_name == '日職聯' )){ 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+
+	if (company_name == 'Egobet') {
+		if ((league_name == '日職聯' )){ 
+			each_data.querySelectorAll("td")[1].style.fontWeight ='bold' ;
+			each_data.querySelectorAll("td")[1].style.backgroundColor ='Red' ;
+
+		}
+
+	}
+
+
+
+
+}
 function libo_data(libo_per,high_init,low_init,libo_index,element_index) {
 	if ( libo_per > high_init*0.985 ) {
 			document.querySelectorAll("#dataList")[0].querySelectorAll("tr")[libo_index].querySelectorAll("td")[element_index].style.color ='Blue';
@@ -84,7 +156,7 @@ function libo_data(libo_per,high_init,low_init,libo_index,element_index) {
 
 function handle_data(odds_data,libo_data,index,element_index,init_per,high_init,low_init,return_rate,win_kelly,tie_kelly,lose_kelly) {
 	difference_gap = odds_data - libo_data;
-	console.log(index);
+	// console.log(index);
 	element_index = element_index + 1 ;
 
 	// if (win_kelly )
